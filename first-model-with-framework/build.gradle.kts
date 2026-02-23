3e415ccbc0c7ea6a873b6fa906bf84d914190981
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-02-23 18:44:25 +0000

    Update versions of Gradle plugins

--- first-model-with-framework/build.gradle.kts
+++ first-model-with-framework/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.core-jvm") version "2.0.0-SNAPSHOT.053"
+    id("io.spine.core-jvm") version "2.0.0-SNAPSHOT.054"
 }
 
 repositories {
--- first-model/build.gradle.kts
+++ first-model/build.gradle.kts
@@ -26,5 +26,5 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.395"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.396"
 }
