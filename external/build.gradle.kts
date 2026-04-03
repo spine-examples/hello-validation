23bc23c5805875648eb578e0a3f9fa15421645e7
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-04-03 19:32:48 +0100

    Bump version -> `2.0.0-SNAPSHOT.411`

--- external/build.gradle.kts
+++ external/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.410"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.411"
 }
 
 spineCompilerRemoteDebug(enabled = false)
