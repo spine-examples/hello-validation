4b79b1dffe5195982c57d0d9af41e7b4f3fccbbc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-05-07 21:44:16 +0100

    Update validation version

--- external/build.gradle.kts
+++ external/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.416"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.417"
 }
 
 spineCompilerRemoteDebug(enabled = false)
