0857c90894b6f4a4caf12408088c6b59768110ce
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-05-14 16:32:17 +0100

    Bump Validation

--- external/build.gradle.kts
+++ external/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.420"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.421"
 }
 
 spineCompilerRemoteDebug(enabled = false)
