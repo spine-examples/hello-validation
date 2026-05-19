4121001dac956ffada79ca26468605cdde99abe4
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-05-19 20:09:37 +0100

    Bump Validation in examples and docs

--- first-model/build.gradle.kts
+++ first-model/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.430"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.431"
 }
 
 //TODO:2026-05-16:alexander.yevsyukov: Remove when CoreJvm upgrades to new Base.
