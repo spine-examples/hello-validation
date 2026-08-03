6c3382f8a2e761c6ebe576578cbf5657e7eb4671
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2026-08-03 15:49:13 +0100

    Update version in examples

--- external/build.gradle.kts
+++ external/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.450"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.461"
 }
 
 spineCompilerRemoteDebug(enabled = false)
--- first-model/build.gradle.kts
+++ first-model/build.gradle.kts
@@ -26,7 +26,7 @@
 
 plugins {
     module
-    id("io.spine.validation") version "2.0.0-SNAPSHOT.450"
+    id("io.spine.validation") version "2.0.0-SNAPSHOT.461"
 }
 
 //TODO:2026-05-16:alexander.yevsyukov: Remove when CoreJvm upgrades to new Base.
